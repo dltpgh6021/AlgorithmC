02e86ca60dcb0806efd9bcb8d0085f648f044a52
dltpgh6021 <seholee7003@gamil.com>
2024-08-10 09:54:36 +0000

    8/10 boj 2747

---
+++ c2747.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	
+	int* arr = calloc(n + 1, sizeof(int));
+	
+	arr[0] = 0;
+	arr[1] = 1;
+	
+	for(int i = 2; i <= n; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	
+	printf("%d", arr[n]);
+	
+	return 0;
+}
